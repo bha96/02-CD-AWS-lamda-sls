e34032deab7c64ea57460ef48ace6a3d5e29a805
bha96 <andreassen.bendik@gmail.com>
2023-09-19 09:21:03 +0000

    changes in lambda

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -3,7 +3,7 @@ import json
 import boto3
 
 def handler(event, context):
-
+    print("Fuckeroo")
     client = boto3.client('comprehend')
     body = event["body"]
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
